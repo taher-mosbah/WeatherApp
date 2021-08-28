748e430a0e9139c74b97df12753333aade2c742f
mohamed mosbah <mohamed.mosbah@adevinta.com>
2021-08-28 19:26:35 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Mohamed Mosbah on 28/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
